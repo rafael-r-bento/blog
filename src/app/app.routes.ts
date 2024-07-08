f795d2400f94eae93f6abca76d24fbfd63f3b7dc
Rafael Bento <rafael.bento@tuta.com>
2024-07-08 15:54:09 -0300

    Remove default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { JSInfoComponent } from './js-info/js-info.component';
 import { PostComponent } from './post/post.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'home/:page', component: PostComponent },
   { path: 'jsinfo', component: JSInfoComponent },
