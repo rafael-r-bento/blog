757370aeae72c7a282f87a847b323272021f370d
Rafael Bento <rafael.bento@tuta.com>
2024-07-08 17:13:09 -0300

    Change router configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withRouterConfig, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideHttpClient()]
+  providers: [provideRouter(routes, withRouterConfig({ onSameUrlNavigation: "reload", }), withComponentInputBinding()), provideHttpClient()]
 };
