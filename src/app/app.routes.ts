1ff334931038265a2edb511f22c08f517c1da82f
Rafael Bento <rafael.bento@tuta.com>
2024-07-09 00:19:56 -0300

    Change route name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,7 @@ import { JSInfoComponent } from './js-info/js-info.component';
 import { PostComponent } from './post/post.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'home/:page', component: PostComponent },
+  { path: '', component: HomeComponent },
+  { path: 'post/:page', component: PostComponent },
   { path: 'jsinfo', component: JSInfoComponent },
-  { path: '**', pathMatch: 'full', component: HomeComponent },
 ];
