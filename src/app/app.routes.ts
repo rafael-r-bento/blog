36bce1a026282ce3aeb581512528a322842ef117
Rafael Bento <rafael.bento@tuta.com>
2024-07-09 01:20:44 -0300

    Remove JSInfoComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { JSInfoComponent } from './js-info/js-info.component';
 import { PostComponent } from './post/post.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'post/:page', component: PostComponent },
-  { path: 'jsinfo', component: JSInfoComponent },
 ];
