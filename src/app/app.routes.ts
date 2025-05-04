ddf7716764689fe993ecf09ecf082c266392e0f1
Rafael Bento <rafael.bentoe.silva@gmail.com>
2025-05-04 18:17:43 -0300

    Remove Snippets component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PostComponent } from './post/post.component';
-import { SnippetsComponent } from './snippets/snippets.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'post/:page', component: PostComponent },
-  { path: 'snippets', component: SnippetsComponent },
   { path: 'contact', component: ContactComponent }
 ];
