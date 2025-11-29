2215d9f8b668b0d122619f1dc5e08c0a3b83f088
Rafael Bento <rafael.bentoe.silva@gmail.com>
2025-11-28 21:51:27 -0300

    Update angular/cli

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]})
   .catch((err) => console.error(err));
